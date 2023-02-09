fce08eb438ff4b45b564acecb828de91aa8e87c7
MarkoVMicic <marko.v.micic@gmail.com>
2023-02-09 13:56:26 +0800

    C loop that sums integers in an array, one at a time.
        Note the compiler pragma directive to prevent loop unrolling.

---
+++ Waste/4-c-sum-array.c
@@ -0,0 +1,61 @@
+#include <windows.h>
+#include <intrin.h>
+
+typedef unsigned int u32;
+typedef unsigned long long int u64;
+#pragma optimize("", off) // Disable optimizations 
+// Do not inline this function
+u32 __declspec(noinline) SingleScalar(  u32 Count,   u32 *Input) 
+{
+    u32 Sum = 0;
+    #pragma loop( no_vector )
+    for(  u32 Index = 0; Index < Count; ++Index)
+    {
+        Sum += Input[Index];
+    }
+    return Sum;
+}
+
+int main(int ArgsCount, char **Args)
+{
+    u32 Count = 4096;
+    u32 Input[4096];
+    for(u32 Index = 0; Index < Count; ++Index)
+    {
+        Input[Index] = Index;
+    }
+
+    u32 TryCount = 100000;
+    u64 MinCyclesElapsed = INT_MAX;
+    u64 BeginCycles;
+    u64 EndCycles;
+    u64 CyclesElapsed;
+    u32 Result;
+
+    for(u32 Try = 0; Try < TryCount; ++Try)
+    {
+        // QueryPerformanceCounter(&BeginCycles);
+        BeginCycles = __rdtsc();
+        SingleScalar(Count, Input);    
+        EndCycles = __rdtsc();
+
+        CyclesElapsed = EndCycles - BeginCycles;
+        if(CyclesElapsed < MinCyclesElapsed)
+        {
+            MinCyclesElapsed = CyclesElapsed;
+        }
+    }
+    double CyclesPerAdd = MinCyclesElapsed / Count;
+    double AddsPerCycle = 1/CyclesPerAdd;
+    printf("Cycles Elapsed: %d\n", MinCyclesElapsed);
+    printf("Cycles/Add: %f\n", CyclesPerAdd);
+    printf("Adds/Cycle: %f\n", AddsPerCycle);
+
+    return 0;
+}
+
+/*
+Cycles Elapsed: 19132
+Cycles/Add:     4
+Adds/Cycle:     0.25
+*/
